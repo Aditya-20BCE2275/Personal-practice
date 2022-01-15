2a567dbf218384faabeb502969f60ef91e6ba418
Aditya-20BCE2275 <amohite7100@gmail.com>
2022-01-15 16:15:43 +0530

    Binary search

---
+++ Binary_search.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+// Binary search 
+// elements should be pressent in sorted order 
+// we compare the mid element of the array. if the array element is > key then we search the 1st half. And if the element < key -> 2nd half. And repeat the process.
+int Bsearch(int arr[],int n, int key)
+{
+    int s=0,e=n;
+    while(s<=e)
+    {
+        int mid=(s+e)/2;
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        else if(arr[mid]>key)
+        {
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int i,n;
+    cout<<"enter the number of the elements u wanna enter"<<endl;
+    cin>>n;
+    int arr[n];
+    for ( i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"enter the key u wanna find"<<endl;
+    int key;
+    cin>>key;
+    cout<<Bsearch(arr,n,key);
+    return 0;
+}
