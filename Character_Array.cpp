2541523e470457fe04f8085d3bad8f98e11067e0
Aditya-20BCE2275 <amohite7100@gmail.com>
2022-01-17 09:09:35 +0530

    Character_arrays

---
+++ Character_Array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Strings in character array end with a null character="\0";
+    // char arr[100] = "apple";
+    // int i=0;
+    // while (arr[i] != '\0')
+    // {
+    //     cout<<arr[i]<<endl;
+    //     i++;
+    // }
+
+    //  Check palindrome in (char array / words
+    int n;
+    cin>>n;
+    char a[n+1];
+    cin>>a;
+    bool check = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i]!=a[n-2-i])
+        {
+            cout<<a[i]<<a[n-2-i];
+            check = 0;
+            break;
+        }        
+    }
+    if (check == 1)
+    {
+        cout<<"word is a palindrome"<<endl;
+    }
+    else
+    cout<<"word in not a palindrome"<<endl;
+    
+    return 0;
+}
