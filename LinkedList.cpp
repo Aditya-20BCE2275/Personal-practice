21045addf0a46266809090421de9b995d68447e9
Aditya-20BCE2275 <amohite7100@gmail.com>
2022-04-04 14:11:23 +0530

    Linkedlist with deletion

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -8,7 +8,7 @@ public:
     int data;
     node *next;
     node(int val)
-    { 
+    {
         data = val;
         next = NULL;
     }
@@ -54,15 +54,43 @@ void display(node *head)
 bool search(node *head, int key)
 {
     node *temp = head;
-    while (temp->next != NULL)
+    while (temp != NULL)
     {
-        if(temp->data==key)
+        if (temp->data == key)
         {
             return true;
         }
-        temp=temp->next;
+        temp = temp->next;
     }
     return false;
+}
+
+void deleteathead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void deletenode(node *&head, int val)
+{
+    node *temp = head;
+    if (head == NULL)
+    {
+        cout << "Linked list is empty" << endl;
+        return;
+    }
+    if (head->next == NULL)
+    {
+        deleteathead(head);
+    }
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
 }
 
 int main()
@@ -74,6 +102,10 @@ int main()
     display(head);
     insertathead(head, 4);
     display(head);
-    cout<<search(head,2)<<endl;
+    cout << search(head, 2) << endl;
+    deletenode(head, 2);
+    display(head);
+    deleteathead(head);
+    display(head);
     return 0;
 }
